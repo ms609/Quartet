ad17d91be989b49dc0201aeb9bf6daf8425c5f29
Martin R. Smith <martins@gmail.com>
2017-11-21 10:17:18 +0000

    Template for tqDist reference

--- R/dist_funcs.R
+++ R/dist_funcs.R
@@ -225,12 +225,7 @@ CompareQuartets <- function (x, cf) {
 #' }
 #' 
 #' @references {
-  #' \cite{SAND, A., HOLT, M. K., JOHANSEN, J., BRODAL, G. S., MAILUND, T. and 
-  #' PEDERSEN, C. N. S. 2014. 
-  #' tqDist: a library for computing the quartet and triplet distances between 
-  #' binary or general trees. \emph{Bioinformatics}, 30, 2079–2080.
-  #' <doi:10.1093/bioinformatics/btu157>
-  #' }
+#' @template refTqDist
 #' }
 #' @export
 MatchingQuartets <- function (trees) {
