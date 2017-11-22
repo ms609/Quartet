439b6166eb9e7e750a161941bf4a3f857d730423
Martin R. Smith <martins@gmail.com>
2017-11-22 15:28:43 +0000

    Functions for partitions too

--- R/TernaryPlot.R
+++ R/TernaryPlot.R
@@ -1,8 +1,19 @@
-#' Quartet plot
+#' Quartet Points
 #' 
-#' Plot how similar trees are to a reference tree.
+#' Generate points to add to a ternary plot
 #' 
 #' @template treesParam
+#' 
+#' @examples {
+#'   TernaryGrid +
+#'   geom_point(data=QuartetPoints(trees), color=rgb(0.3, 0.2, 0.5), shape=3, size=3) +
+#'   ggtitle(paste0("Quartets:"))
+#' 
+#' 
+#'   TernaryGrid +
+#'   geom_point(data=SplitsPoints(trees), color=rgb(0.3, 0.2, 0.5), shape=3, size=3) +
+#'   ggtitle(paste0("Partitions:"))
+#' }
 #' 
 #' @references {
 #'   #TODO Provide ggtern citation.
@@ -11,26 +22,37 @@
 #' @author Martin R. Smith
 #' 
 #' @importFrom ggtern ggtern
-#' @importFrom ggplot2 geom_line
+#' @importFrom ggplot2 geom_line ggtitle
 #' @export
-QuartetPlot <- function (trees) {
-  ref_tree <- trees[[1]]
+QuartetPoints <- function (trees) {
   status <- MatchingQuartets(trees)
   status <- rbind(status, status[1] - colSums(status))
-  ccu <- c('Consistent', 'Unresolved', 'Contradicted')
-  rownames(status) <- ccu
-  ggtern(mapping=aes(Contradicted, Consistent, Unresolved)) +
-    geom_path
+  
+  # Return: 
+  data.frame(Consistent   = status[1, ], 
+             Contradicted = status[3, ],
+             Unresolved   = status[2, ])
 }
 
+#' @describeIn QuartetPoints Uses partition distance instead of quartet metric.
+SplitsPoints <- function (trees) {
+  status <- MatchingSplits(trees)
+  status <- rbind(status, status[1] - colSums(status))
+  
+  # This probably assumes that ref is bifurcating.  #TODO Add resilience
+  # Return: 
+  data.frame(Consistent   = status['ref', ] - status['ref_not_cf', ], 
+             Contradicted = status['cf_not_ref', ],
+             Unresolved   = status['ref_not_cf', ] - status['cf_not_ref', ])
+}
 
 GREY <- rgb(0.8, 0.8, 0.6, 0.7)
 
 #' @describeIn TernaryGrid Plot KL divergence lines
 #' @export
 #' @keywords internal
-KLDivergenceLine <- function (i) geom_line(data=data.frame(Correct    = c(10-i, 10-(2*i)), 
-                                                           Incorrect  = c(i, 0),
+KLDivergenceLine <- function (i) geom_line(data=data.frame(Consistent    = c(10-i, 10-(2*i)), 
+                                                           Contradicted  = c(i, 0),
                                                            Unresolved = c(0, i*2)),
                                            color=rgb(1, 1, 1, 1))
 
@@ -50,3 +72,15 @@ TernaryGrid <- ggtern(mapping=aes(Contradicted, Consistent, Unresolved)) +
                              Contradicted  = c(0, 1, 0, 0), 
                              Unresolved    = c(0, 0, 1, 0)), color=GREY) +
   theme_rotate(degrees=30)
+
+
+
+
+
+TernaryEdges <- function (x) data.frame(Correct    = x['edges.agree', ],
+                                        Incorrect  = x['edges.conflict', ],
+                                        Unresolved = gen.nedge - x['edges.present', ])
+TernaryQuart <- function (x) data.frame(Correct    = x['quartet.agree', ],
+                                        Incorrect  = x['quartet.conflict', ],
+                                        Unresolved = x['quartet.missing', ])
+
