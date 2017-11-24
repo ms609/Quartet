8c8837b3f9f04c4ab43590bae1c8780300b6e421
Martin R. Smith <martins@gmail.com>
2017-11-24 17:01:15 +0000

    Use template for Estabrook type return

--- R/QuartetDistance.R
+++ R/QuartetDistance.R
@@ -196,18 +196,9 @@ QuartetStates <- function (splits) {
 #'
 #' Compares each quartet in a list, calculating how many statements are identical
 #'  in both lists.
-#' @return {
-#'   Returns an array of six numberic elements, each corresponding to the quantities of 
-#'   Estabrook _et al_. (1985):
-#'   \describe{
-#'   \item{Q}{The total number of quartets for _n_ tips.}
-#'   \item{s}{The number of quartets that are resolved identically in both trees.}
-#'   \item{d}{The number of quartets that are resolved differently in each tree.}
-#'   \item{r1}{The number of quartets that are resolved in tree 1, but not in tree 2.}
-#'   \item{r2}{The number of quartets that are resolved in tree 2, but not in tree 1.}
-#'   \item{u}{The number of quartets that are unresolved in both trees.}
-#'   }
-#' }
+#' 
+#' @templateVar quartets quartets
+#' @template returnEstabrook
 #'
 #' @author Martin R. Smith
 #'
