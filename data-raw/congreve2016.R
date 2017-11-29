1f8e22613da7cb17f7919383a2210eb434549e6b
Martin R. Smith <martins@gmail.com>
2017-11-29 10:50:01 +0000

    Add strict consensus of iw2-10

--- data-raw/congreve2016.R
+++ data-raw/congreve2016.R
@@ -25,6 +25,7 @@ for (NUM in FILE_NUMS) {
   }
 }
 
+TREE_FILE <- paste0(DIR_ROOT, 'Trees/%s/%s.', FILE_NUMS[-34], '%s.con.nex')
 # Load consensus trees from Equal Weights and Markov model analyses
 markov   <- lapply(sprintf(TREE_FILE, 'mk', 'mk', ''), read.nexus)
 equal <- LoadSuboptimal('eq')
@@ -33,6 +34,9 @@ imp2  <- LoadSuboptimal('k2')
 imp3  <- LoadSuboptimal('k3')
 imp5  <- LoadSuboptimal('k5')
 impX  <- LoadSuboptimal('kX')
+impC  <- lapply(seq_along(imp2),
+                function(i) lapply(1:21, 
+                                   function (j) consensus(imp2[[i]][[j]], imp3[[i]][[j]], imp5[[i]][[j]], impX[[i]][[j]])))
 
 BLANK_RETURN <- matrix(0, ncol=21, nrow=6)
 
@@ -43,7 +47,8 @@ clQuartets <- list(
   implied2  = vapply(imp2,   MatchingQuartets, cf=referenceTree, BLANK_RETURN),
   implied3  = vapply(imp3,   MatchingQuartets, cf=referenceTree, BLANK_RETURN),
   implied5  = vapply(imp5,   MatchingQuartets, cf=referenceTree, BLANK_RETURN),
-  implied10 = vapply(impX,   MatchingQuartets, cf=referenceTree, BLANK_RETURN)
+  implied10 = vapply(impX,   MatchingQuartets, cf=referenceTree, BLANK_RETURN),
+  impliedC  = vapply(impC,   MatchingQuartets, cf=referenceTree, BLANK_RETURN)
 )
 
 clPartitions <- list(
@@ -53,7 +58,8 @@ clPartitions <- list(
   implied2  = vapply(imp2,   MatchingSplits, cf=referenceTree, BLANK_RETURN),
   implied3  = vapply(imp3,   MatchingSplits, cf=referenceTree, BLANK_RETURN),
   implied5  = vapply(imp5,   MatchingSplits, cf=referenceTree, BLANK_RETURN),
-  implied10 = vapply(impX,   MatchingSplits, cf=referenceTree, BLANK_RETURN)
+  implied10 = vapply(impX,   MatchingSplits, cf=referenceTree, BLANK_RETURN),
+  impliedC  = vapply(impC,   MatchingSplits, cf=referenceTree, BLANK_RETURN)
 )
 
 devtools::use_data(clQuartets, clPartitions, overwrite=TRUE)
