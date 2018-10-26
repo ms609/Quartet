b4b14ffa74405b1bdadcba47f41854fb7ae35065
Martin Smith <martins@gmail.com>
2018-10-26 17:08:13 +0100

    Detail

--- R/ReleaseQuestions.R
+++ R/ReleaseQuestions.R
@@ -1,7 +1,7 @@
 release_questions <- function() {
   c(
     "Is inst/CITATION up to date?",
-    "Have you updated REFERENCES.bib?",
+    "Have you updated REFERENCES.bib with a citation to the published study?",
     "Have you updated .zenodo.json?"
   )
 }
