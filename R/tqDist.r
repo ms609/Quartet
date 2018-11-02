c707366f2846c72f023c586808ded653b26e8c23
Martin Smith <martins@gmail.com>
2018-11-02 17:14:45 +0000

    Save tqDist bindings

---
+++ R/tqDist.r
@@ -0,0 +1,72 @@
+#' Triplet and quartet distances with tqDist
+#' 
+#' Functions to calculate triplet and quartet distances between pairs of trees.
+#' 
+#' @param file,file1,file2 Paths to files containing a tree or trees in Newick format.
+#' 
+#' @return The distance between the requested trees.
+#' 
+#' @author Martin R. Smith, after Andreas Sand
+#' 
+#' @references \insertRef{Sand2014}{Quartet}
+#' @export
+QuartetDistance <- function(file1, file2) {
+  ValidateQuartetFile(file1)
+  ValidateQuartetFile(file2)
+  .Call('_Quartet_tqdist_QuartetDistance', as.character(file1), as.character(file2));
+}
+
+#' @describeIn QuartetDistance Distance between pairs
+PairsQuartetDistance <- function(file1, file2) {
+  ValidateQuartetFile(file1)
+  ValidateQuartetFile(file2)
+  .Call('_Quartet_tqdist_PairsQuartetDistance', as.character(file1), as.character(file2));
+}
+
+#' @describeIn QuartetDistance Distance between all pairs
+AllPairsQuartetDistance <- function(file) {
+  ValidateQuartetFile(file)
+  .Call('_Quartet_tqdist_AllPairsQuartetDistance', as.character(file));
+}
+
+#' @describeIn QuartetDistance Triplet distance between trees
+TripletDistance <- function(file1, file2) {
+  ValidateQuartetFile(file1)
+  ValidateQuartetFile(file2)
+  .Call('_Quartet_tqdist_TripletDistance', as.character(file1), as.character(file2));
+}
+
+#' @describeIn QuartetDistance Triplet distance between pairs
+PairsTripletDistance <- function(file1, file2) {
+  ValidateQuartetFile(file1)
+  ValidateQuartetFile(file2)
+  .Call('_Quartet_tqdist_PairsTripletDistance', as.character(file1), as.character(file2));
+}
+
+#' @describeIn QuartetDistance Triplet distance between all pairs
+AllPairsTripletDistance <- function(file) {
+  ValidateQuartetFile(file)
+  .Call('_Quartet_tqdist_AllPairsTripletDistance', as.character(file));
+}
+
+#' Validate filenames
+#' 
+#' Verifies that file parameters are character strings describing files that exist
+#' 
+#' @param file Variable to validate
+#' 
+#' @return `TRUE` if `file` is a character vector of length one describing 
+#'   a file that exists, a fatal error otherwise.
+#' 
+#' @author Martin R. Smith
+#' 
+#' @export
+#' @keywords internal
+ValidateQuartetFile <- function (file) {
+  if (length(file) != 1) {
+    stop("file must be a character vector of length one")
+  }
+  if (!file.exists(file)) {
+    stop("file ", file, " not found")
+  }
+}
