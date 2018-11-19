80be6559dba6552578d38417cd31bcfae1f0b3e8
Martin Smith <martins@gmail.com>
2018-11-19 17:12:19 +0000

    Use usethis

--- data-raw/sq_trees.R
+++ data-raw/sq_trees.R
@@ -1,6 +1,6 @@
 set.seed(1) # 1 produces a random tree with a Qt dist of 226 ~= 330 * 2 / 3
 ref_tree <- ape::read.tree(text="(((1, 2), 3), (((4, 5), 6), ((7, (8, 9)), (10, 11))));")
-sq_trees <- list (
+sq_trees <- structure(list (
   ref_tree      = ref_tree,
   move_one_near = ape::read.tree(text="(((2, 3), 1), (((4, 5), 6), ((7, (8, 9)), (10, 11))));"),
   move_one_mid  = ape::read.tree(text="((2, 3), ((((4, 5), 1), 6), ((7, (8, 9)), (10, 11))));"),
@@ -16,6 +16,5 @@ sq_trees <- list (
   m2mid_colsome = ape::read.tree(text="(((1, 2), 3), (4, (10, 11), 5, 6, 7, 8, 9));"),
   opposite_tree = ape::read.tree(text="(((1, 11), 3), (((4, 9), 6), ((10, (8, 2)), (5, 7))));"),
   random_tree   = ape::rtree(n_tip, tip.label=seq_len(n_tip), br=NULL)
-)
-class(sq_trees) <- 'multiPhylo'
-devtools::use_data(sq_trees)
+), class = 'multiPhylo')
+usethis::use_data(sq_trees)
