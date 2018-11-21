795a27fe9973af4b4c2ce3ffc7d67cf104ddf6da
Martin Smith <martins@gmail.com>
2018-11-21 10:49:14 +0000

    SplitPoints with unresolved comparison tree

--- R/TernaryPoints.R
+++ R/TernaryPoints.R
@@ -4,9 +4,15 @@
 #' and similarity) on a ternary plot.
 #' 
 #' @template treesParam
+#' @template treesCfParam
 #' @return A data frame listing the ternary coordinates of trees, based on the
-#'         amount of information that they have in common with the first member 
-#'         of the list.
+#'         amount of information that they have in common with the comparison
+#'         tree (which defaults to the first member of the list, if unspecified).
+#'         
+#'         If the comparison tree `cf` contains polytomies, then it is possible
+#'         that trees within `trees` will be more resolved than `cf`.  A split
+#'         that is resolved in `trees[[i]]` but not in `cf` will contribute 
+#'         `+1` to the `Contradicted` count, and `-1` to the `Unresolved` count.
 #' 
 #' @examples {
 #'   library('Ternary')
@@ -24,7 +30,7 @@
 #' @author Martin R. Smith
 #' 
 #' @export
-QuartetPoints <- function (trees) {
+QuartetPoints <- function (trees, cf = trees[[1]]) {
   status <- QuartetStatus(trees)
   
   # Return: 
@@ -35,16 +41,13 @@ QuartetPoints <- function (trees) {
 
 #' @describeIn QuartetPoints Uses partition distance instead of quartet metric.
 #' @export
-SplitPoints <- function (trees) {
-  status <- SplitStatus(trees)
-  # TODO Delete following line (?)
-  # status <- rbind(status, status[1] - colSums(status))
-  
-  # This probably assumes that ref is bifurcating.  #TODO Add resilience
-  # Return: 
+SplitPoints <- function (trees, cf = trees[[1]]) {
+  status <- SplitStatus(trees, cf)
+
+    # Return: 
   data.frame(Unresolved   = status[, 'ref_not_cf'] - status[, 'cf_not_ref'],
              Contradicted = status[, 'cf_not_ref'],
-             Consistent   = status[, 'ref'] - status[, 'ref_not_cf'])
+             Consistent   = status[, 'cf_and_ref'])
 }
 #' @rdname QuartetPoints
 #' @export
--- tests/testthat/test-4-TernaryPoints.R
+++ tests/testthat/test-4-TernaryPoints.R
@@ -2,7 +2,12 @@ context("Plotting functions")
 
 data('sq_trees')
 
-test_that("Quartets are counted correctly", {
+test_that("Splits are compared", {
   expect_equal(c(15, 3), dim(QuartetPoints(sq_trees)))
-  expect_equal(c(15, 3), dim(SplitPoints(sq_trees)))
+  expect_identical(330, unique(rowSums(QuartetPoints(sq_trees))))
+  expect_equal(c(15, 3), dim(BipartitionPoints(sq_trees)))
+  expect_identical(8, unique(rowSums(BipartitionPoints(sq_trees))))
+  expect_equal(c(0,0,8, 0,1,7, 0,3,5, 0,4,4, 0,1,7, 0,2,6, 0,3,5,
+                 1,0,7, 5,0,3, 1,3,4, 3,3,2, 1,2,5, 5,0,3, 0,8,0, 0,8,0), as.integer(unlist(t(SplitPoints(sq_trees)))))
+  expect_equal(c(15, 3), dim(BipartitionPoints(sq_trees, cf=sq_trees$collapse_some)))
 })
