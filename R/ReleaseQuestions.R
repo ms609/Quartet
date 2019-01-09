4ebef3015bf90878ea9c88d457d192bf3067bf87
Martin Smith <martins@gmail.com>
2019-01-09 16:22:10 +0000

    Update CITATION

--- R/ReleaseQuestions.R
+++ R/ReleaseQuestions.R
@@ -2,7 +2,6 @@ release_questions <- function() {
   c(
     "Is the code free of #TODOs?",
     "Have you updated REFERENCES.bib with a citation to the published study?",
-    "Have you updated inst/CITATION with a citation to the published study?",
     "Have you updated .zenodo.json?"
   )
 }
