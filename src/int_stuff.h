6ef43ba8eab1f5a67ff461a4643f07bbc2f3470f
Martin Smith <martins@gmail.com>
2019-01-21 10:58:52 +0000

    Use int_fast64_t on all platforms
    Requires C++11

--- src/int_stuff.h
+++ src/int_stuff.h
@@ -2,15 +2,9 @@
   
   #define INT_STUFF_H
   
-  #include <iostream>
-  #include <stdint.h>
+  #include <cstdint>
   
-  #ifdef _WIN32
-    #define INTTYPE_N4 __int64 //previously long long, which fails -pedantic
-    #define INTTYPE_REST __int64 //previously long long, which fails -pedantic
-  #else
-    #define INTTYPE_N4 __int64_t //previously long long, which fails -pedantic
-    #define INTTYPE_REST __int64_t //previously long long, which fails -pedantic
-  #endif
-  
+  #define INTTYPE_N4 int_fast64_t // long long fails -pedantic
+  #define INTTYPE_REST int_fast64_t // long long fails -pedantic
+
 #endif
