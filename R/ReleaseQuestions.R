08414ad1ddca549ff8e9ae63dee4fb9f6761da73
Martin Smith <martins@gmail.com>
2019-01-23 09:53:22 +0000

    Add DOI to citation

--- R/ReleaseQuestions.R
+++ R/ReleaseQuestions.R
@@ -1,7 +1,7 @@
 release_questions <- function() {
   c(
     "Is the code free of #TODOs?",
-    "Have you updated REFERENCES.bib with a citation to the published study?",
+    "Have you updated REFERENCES.bib and README.md with a citation to the published study?",
     "Have you updated .zenodo.json?"
   )
 }
