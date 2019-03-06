42728415c92ca1aa49f34235412ced0f625fe03b
Martin Smith <martins@gmail.com>
2019-03-06 10:42:03 +0000

    Prep for release

--- R/ReleaseQuestions.R
+++ R/ReleaseQuestions.R
@@ -1,7 +1,6 @@
 release_questions <- function() {
   c(
     "Is the code free of #TODOs?",
-    "Have you updated REFERENCES.bib and README.md with a citation to the published study?",
     "Have you updated .zenodo.json?"
   )
 }
