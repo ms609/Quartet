c16f5d152e8f9359864b72436823aafd66136c9b
Martin Smith <martins@gmail.com>
2019-03-06 10:00:27 +0000

    Additional tree examples

--- data-raw/sq_trees.R
+++ data-raw/sq_trees.R
@@ -16,6 +16,9 @@ sq_trees <- structure(list (
   m2mid_col1    = ape::read.tree(text="(((1, 2), 3), ((((4, 5), (10, 11)), 6), (7, 8, 9)));"),
   m2mid_colsome = ape::read.tree(text="(((1, 2), 3), (4, (10, 11), 5, 6, 7, 8, 9));"),
   opposite_tree = ape::read.tree(text="(((1, 11), 3), (((4, 9), 6), ((10, (8, 2)), (5, 7))));"),
+  caterpillar   = ape::read.tree(text="((((((1, 2), 3), 4), 5), 6), (7, (8, (9, (10, 11)))));"),
+  top_and_tail  = ape::read.tree(text="((((((11, 2), 3), 4), 5), 6), (7, (8, (9, (10, 1)))));"),
+  anti_pectinate = ape::read.tree(text="(9,((4,(6,((3,(1,10)),8))),(((2,11),5),7)));"),
   random_tree   = ape::rtree(n_tip, tip.label=seq_len(n_tip), br=NULL)
 ), class = 'multiPhylo')
 usethis::use_data(sq_trees, overwrite=TRUE, compress='xz')
