ab829afcfed2dccadc6e5c917e705f9532209a6f
Martin Smith <martins@gmail.com>
2019-11-25 15:13:33 +0000

    Streamline check

--- tests/testthat/valgrind.R
+++ tests/testthat/valgrind.R
@@ -3,7 +3,7 @@ library(pkgload)
 load_all()
 getwd()
 dir()
-setwd('testthat')
+if (dir.exists('testthat')) setwd('testthat/tests')
 testFiles <- list.files(pattern = 'test\\-.*\\.R', full.names= TRUE)
 # No doppleganger testing in interactive mode
 lapply(testFiles[-c(3, 6)], source)
