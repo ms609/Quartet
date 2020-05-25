7aad9b3afddc3b34e84715615b1224282bb707e4
Martin Smith <martins@gmail.com>
2020-05-25 11:26:19 +0100

    c++11 required.

--- src/int_stuff.h
+++ src/int_stuff.h
@@ -2,13 +2,8 @@
   
   #define INT_STUFF_H
   
-#if __cplusplus <= 199711L
-  #define INTTYPE_N4 long int // long long fails -pedantic
-  #define INTTYPE_REST long int // long long fails -pedantic
-#else 
   #include <cstdint>
   #define INTTYPE_N4 int_fast64_t // long long fails -pedantic
   #define INTTYPE_REST int_fast64_t // long long fails -pedantic
 
-#endif
 #endif
