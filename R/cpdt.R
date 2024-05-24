69b679bae950140070d62a0287feb934707a7cf3
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2024-05-24 12:11:45 +0100

    Use edges not files

--- R/cpdt.R
+++ R/cpdt.R
@@ -29,17 +29,8 @@ CPDTDist <- function (tree1, tree2) {
   if (!inherits(tree2, "phylo")) {
     stop("`tree2` must be of class phylo")
   }
-  t1 <- tree1
-  t2 <- RenumberTips(tree2, tree1)
-  t1[["tip.label"]] <- t2[["tip.label"]] <- seq_len(NTip(t1))
   
-  file1 <- tempfile("tree-", fileext = ".nwk")
-  write.tree(t1, file = file1)
-  on.exit(unlink(file1))
-  file2 <- tempfile("tree-", fileext = ".nwk")
-  write.tree(t2, file = file2)
-  on.exit(unlink(file2), add = TRUE)
-  
-  .Call("_Quartet_cpdt_dist_file", file1, file2)
+  .Call("_Quartet_cpdt_pair", tree1[["edge"]],
+        RenumberTips(tree2, tree1)[["edge"]])
 }
 
