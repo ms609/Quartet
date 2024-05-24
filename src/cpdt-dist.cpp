8e5b49aeab364c9f855ad1412143a3b8e5d69a2d
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2024-05-24 10:47:58 +0100

    First draft at parsing R tree

--- src/cpdt-dist.cpp
+++ src/cpdt-dist.cpp
@@ -1,5 +1,5 @@
 #include <Rcpp.h>
-using namespace Rcpp;
+#include <TreeTools/renumber_tree.h> /* for preorder */
 
 #include <cstdlib>
 #include <iostream>
@@ -13,6 +13,7 @@ using namespace Rcpp;
 #define NDEBUG
 
 using namespace std;
+using namespace Rcpp;
 
 
 
@@ -58,4 +59,51 @@ IntegerVector cpdt_dist_file(CharacterVector file1,
     }
 
     return IntegerVector::create(result);
+}
+
+
+
+tree_node* parse_tree_support(const IntegerMatrix& edge, 
+                              const int n_tip, int* row,
+                              std::vector<tree_node*>& nodes) {
+    
+    int vecpos = nodes.size();
+    nodes.push_back(new tree_node(vecpos));
+    
+    if (*row < edge.nrow()) {
+        tree_node* subtree;
+        const int child = edge(*row, 1);
+        ++(*row);
+        if (child > n_tip) {
+            // Child is a new node, which will occur on the next row
+            subtree = parse_tree_support(edge, n_tip, row, nodes);
+        } else {
+            // Child is a leaf
+            subtree = new tree_node(nodes.size(), child - 1);
+            nodes.push_back(subtree);
+        }
+        nodes[vecpos]->add_child(subtree);
+    }
+    
+    return nodes[vecpos];
+}
+
+
+tree* parse_edge(const IntegerVector& parent, const IntegerVector& child) {
+    
+    IntegerMatrix edge = TreeTools::preorder_edges_and_nodes(parent, child);
+    const int n_tip = edge(0, 0) - 1;
+    int row = 0;
+    std::vector<tree_node*> nodes;
+    
+    parse_tree_support(edge, n_tip, &row, nodes);
+    
+    tree* mytree = new tree(nodes);
+    return mytree;
+}
+
+// [[Rcpp::export]]
+IntegerVector cpdt_tree(const IntegerVector parent, const IntegerVector child) {
+    tree* mytree = parse_edge(parent, child);
+    return IntegerVector(mytree->get_leaves_num());
 }
